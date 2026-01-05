654ee0f81f6c84e869aa6b5fa50fe09017caaba5
mhmdsalah <ms.mhmdsalah@gmail.com>
2026-01-05 02:41:21 +0200

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Pages
-import LandingPage from './pages/LandingPage';
-import HomePage from './pages/HomePage';
-import TouristCitiesPage from './pages/TouristCitiesPage';
-import TouristActivitiesPage from './pages/TouristActivitiesPage';
-import HotelsPage from './pages/HotelsPage';
-import RestaurantsPage from './pages/RestaurantsPage';
-import TransportationPage from './pages/TransportationPage';
-import EventsPage from './pages/EventsPage';
-import ShoppingPage from './pages/ShoppingPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import SupportPage from './pages/SupportPage';
-import SmartTripPlannerPage from './pages/SmartTripPlannerPage';
-import ReviewsPage from './pages/ReviewsPage';
+import LandingPage from "./pages/landing/Landing";
+// import HomePage from './pages/HomePage';
+// import TouristCitiesPage from './pages/TouristCitiesPage';
+// import TouristActivitiesPage from './pages/TouristActivitiesPage';
+// import HotelsPage from './pages/HotelsPage';
+// import RestaurantsPage from './pages/RestaurantsPage';
+// import TransportationPage from './pages/TransportationPage';
+// import EventsPage from './pages/EventsPage';
+// import ShoppingPage from './pages/ShoppingPage';
+// import LoginPage from './pages/LoginPage';
+// import RegisterPage from './pages/RegisterPage';
+// import SupportPage from './pages/SupportPage';
+// import SmartTripPlannerPage from './pages/SmartTripPlannerPage';
+// import ReviewsPage from './pages/ReviewsPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<HomePage />} />
+        {/* <Route path="/home" element={<HomePage />} />
         <Route path="/cities" element={<TouristCitiesPage />} />
         <Route path="/activities" element={<TouristActivitiesPage />} />
         <Route path="/hotels" element={<HotelsPage />} />
@@ -34,11 +34,10 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/support" element={<SupportPage />} />
         <Route path="/planner" element={<SmartTripPlannerPage />} />
-        <Route path="/reviews" element={<ReviewsPage />} />
+        <Route path="/reviews" element={<ReviewsPage />} /> */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Landing = () => {
+  return <div className="bg-red-500">Landing Page</div>;
+};
+
+export default Landing;
